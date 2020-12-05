2a0b257a1c2e12527e77935d6698f5ed80e02aa6
ConnorStine <49596244+ConnorStine@users.noreply.github.com>
2020-12-04 19:46:53 -0600

    Add more test cases

--- src/test/java/org/apache/commons/mail/TemplateTest.java
+++ src/test/java/org/apache/commons/mail/TemplateTest.java
@@ -17,4 +17,38 @@ public class TemplateTest extends TestCase{
 		assertEquals("a@b.com", testEmail.getBccAddresses().get(0).toString());
 	}
 
+	//addCc
+	@Test
+	public void testAddCc() throws EmailException{
+		//Add an email and see if it was added correctly
+		testEmail.addCc("a@b.com");
+		assertEquals("a@b.com", testEmail.getCcAddresses().get(0).toString());
+	}
+	//addHeader
+	@Test
+	public void testAddHeader() throws IllegalArgumentException{
+		//Check for exceptions to be thrown if either field is empty
+		try {
+			testEmail.addHeader("", "a");
+			fail("Exception not thrown for empty name.");
+		}
+		catch(IllegalArgumentException e){}
+		try {
+			testEmail.addHeader("a", "");
+			fail("Exception not thrown for empty value.");
+		}
+		catch(IllegalArgumentException e){}
+		//Add a header and check that it was mapped correctly
+		testEmail.addHeader("name", "value");
+		assertNotNull("The name should not be null.", testEmail.headers.get("name"));
+		assertEquals("value", testEmail.headers.get("name"));
+	}
+	//addReplyTo
+	@Test
+	public void testAddReplyTo() throws EmailException{
+		//Add a reply and check that it was added to the list correctly
+		testEmail.addReplyTo("a@b.com", "name");
+		assertEquals("a@b.com", testEmail.replyList.get(0).getAddress());
+	}
+
 }
