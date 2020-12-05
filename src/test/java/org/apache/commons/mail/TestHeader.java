8e21690590ff13ebde1e605ad389e7c6bd3ea738
ConnorStine <49596244+ConnorStine@users.noreply.github.com>
2020-12-04 19:59:00 -0600

    Add TestHeader class

--- src/test/java/org/apache/commons/mail/TestHeader.java
+++ src/test/java/org/apache/commons/mail/TestHeader.java
@@ -6,24 +6,12 @@ import org.apache.commons.mail.SimpleEmail;
 
 import junit.framework.TestCase;
 
-public class TemplateTest extends TestCase{
+public class EmailTest extends TestCase{
 	Email testEmail;
 	public void setUp() {
 		testEmail = new SimpleEmail();
 	}
-	public void testAddBcc() throws EmailException {
-		System.out.println("This is a message from the template test.");
-		testEmail.addBcc("a@b.com");
-		assertEquals("a@b.com", testEmail.getBccAddresses().get(0).toString());
-	}
-
-	//addCc
-	@Test
-	public void testAddCc() throws EmailException{
-		//Add an email and see if it was added correctly
-		testEmail.addCc("a@b.com");
-		assertEquals("a@b.com", testEmail.getCcAddresses().get(0).toString());
-	}
+	
 	//addHeader
 	@Test
 	public void testAddHeader() throws IllegalArgumentException{
